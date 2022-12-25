b8698501868717dc99ae64a01902cc1eb6ad8336
ASHIF SHAIKH <ashif1000@gmail.com>
2022-12-26 01:33:09 +0530

    Added views

--- dotNetCore/Controllers/HomeController.cs
+++ dotNetCore/Controllers/HomeController.cs
@@ -1,9 +1,6 @@
-﻿using dotNetCore.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,31 +8,17 @@ namespace dotNetCore.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
-        {
-            return "First MVC controller first method";
-        }
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index1()
+        public ViewResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public ViewResult AboutUs()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public ViewResult ContactUs()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
